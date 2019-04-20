0e8ba499d328d3bfbaa2fae3ae003d1506039f56
Leandro Perez <leandropremier@gmail.com>
2019-04-20 17:03:25 -0300

    Commit Inicial

---
+++ app/src/main/java/br/edu/ifsp/scl/filmesomdbapi/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.edu.ifsp.scl.filmesomdbapi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=56c7e1ad
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
