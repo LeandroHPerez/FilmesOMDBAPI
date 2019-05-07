de9c5bada19a78dbd8bedc0a607d63fb415bda35
Leandro Perez <leandropremier@gmail.com>
2019-05-06 21:24:11 -0300

    Configuração Retrofit e click botão Fragment

--- app/src/main/java/br/edu/ifsp/scl/filmesomdbapi/ModoIdFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/filmesomdbapi/ModoIdFragment.kt
@@ -4,6 +4,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import br.edu.ifsp.scl.appfilmes.ModoFilmeFragment
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -13,6 +15,15 @@ class ModoIdFragment: ModoFilmeFragment() {
         val layoutFragment = inflater.inflate(R.layout.fragment_modo_id, null)
 
         activity?.toolbar?.subtitle = "Modo id"
+
+        // Obtendo a view do layout
+        val edit_text_id_filme = layoutFragment.findViewById<TextView>(R.id.edit_text_id_filme)
+        val buscarButton = layoutFragment.findViewById<TextView>(R.id.buscarButton)
+
+        buscarButton.setOnClickListener(){
+            //Mostrar confirmação de click
+            Toast.makeText(layoutFragment.context,"Clicou no botão", Toast.LENGTH_SHORT).show()
+        }
 
         return layoutFragment
     }
--- app/src/main/java/br/edu/ifsp/scl/filmesomdbapi/ModoTituloFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/filmesomdbapi/ModoTituloFragment.kt
@@ -5,15 +5,27 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import br.edu.ifsp.scl.appfilmes.ModoFilmeFragment
 import kotlinx.android.synthetic.main.toolbar.*
 
 class ModoTituloFragment: ModoFilmeFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        //Inflando a view
         val layoutFragment = inflater.inflate(R.layout.fragment_modo_titulo, null)
 
         activity?.toolbar?.subtitle = "Modo título"
+
+        // Obtendo a view do layout
+        val edit_text_id_filme = layoutFragment.findViewById<TextView>(R.id.edit_text_titulo_filme)
+        val buscarButton = layoutFragment.findViewById<TextView>(R.id.buscarButton)
+
+        buscarButton.setOnClickListener(){
+            //Mostrar confirmação de click
+            Toast.makeText(layoutFragment.context,"Clicou no botão",Toast.LENGTH_SHORT).show()
+        }
 
         return layoutFragment
     }
